f9bb7ec950d9a802918a178f01420c50bdf31a50
OwenBruck <145162288+OwenBruck@users.noreply.github.com>
2023-10-03 14:59:47 -0600

    f

--- 001Multicolored grid/sketch.js
+++ 001Multicolored grid/sketch.js
@@ -10,6 +10,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   document.addEventListener("contextmenu", event => event.preventDefault());
   noStroke();
+  //calls the draw function during startup
   drawGrid();
 }
 
@@ -17,30 +18,37 @@ function draw() {
   gridSize();
 }
 
+
 function gridSize(){
+  //if the right mouse is pressed it wil zoom out and draw
+  //if the left mouse is pressed it wil zoom in and draw
   if (mouseIsPressed) {  
     if (mouseButton === LEFT) {
       gridSpacing = gridSpacing + 2;
     }
     if (mouseButton === RIGHT) {
-      gridSpacing = gridSpacing - 2;
+      if (gridSpacing>8){
+        gridSpacing = gridSpacing - 2;
+      }
     }
     drawGrid();
   }
 }
 
 function keyPressed(){
-  
+  //redraws grid when a key is pressed
   drawGrid();
 }
   
 
 
 function drawGrid(){
+  //draw background to cover extra bits on edge when zooming
   background(255);
+  //draws the grid squares as long as the x value is less than the width-square size
   for (let x = 0; x<width; x = x+gridSpacing){
     for(let y = 0; y<height; y = y + gridSpacing){
-      if(x>width-gridSpacing){
+      if(x>width-gridSpacing||y>height-gridSpacing){
         noFill();
         noStroke();
       
