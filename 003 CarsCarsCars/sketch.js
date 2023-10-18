24533eae1c306800618c93aecaafef60e7f28781
OwenBruck <145162288+OwenBruck@users.noreply.github.com>
2023-10-18 15:15:43 -0600

    d

--- 003 CarsCarsCars/sketch.js
+++ 003 CarsCarsCars/sketch.js
@@ -1,18 +1,34 @@
-// Project Title
-// Your Name
-// Date
+// CarsCarsCars
+// Owen
+// 10/18/23
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// A program where cars and trucks travel on a 2 way road
 
+//globals
+let topLane = [];
+let bottomLane = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for(let i = 0; i<20;i++){
+    topLane.push(new Vehicle(0,Math.floor(random(0,2)),random(0,width),random(height*0.2,height/2-25)));
+  }
+  for(let i = 0; i<20;i++){
+    bottomLane.push(new Vehicle(1,Math.floor(random(0,2)),random(0,width),random(height*0.8-25,height/2+5)));
+  }
+
 }
 
 function draw() {
   background(220);
   drawRoad();
+  for(let t of topLane){
+    t.action();
+  }
+  for(let b of bottomLane){
+    b.action();
+  }
+
 }
 
 function drawRoad(){
@@ -35,17 +51,53 @@ class Vehicle{
     this.x=x;
     this.y=y;
     this.color= color(random(0,255),random(0,255),random(0,255));
-    this.s = random(0,40);
+    this.s = random(5,20);
   }
   display(){
   //draw vehicle baces on type
+    strokeWeight(1);
+    stroke("black");
+    if(this.type ===1){
+      fill(this.color);
+      rect(this.x, this.y, this.x+20, this.y +12);
+      if(this.derection === 0){
+        triangle(this.x,this.y,this.x,this.y+12, this.x-8, this.y+6);
+      }
+      else if(this.derection === 1){
+        triangle(this.x+20,this.y,this.x+20,this.y+12, this.x+28, this.y+6);
+      }
+    }
+    else if(this.type ===0){
+      fill(this.color);
+      rect(this.x, this.y, this.x+30, this.y +20);
+      if(this.derection === 0){
+        circle(this.x,this.y+10,20);
+      }
+      else if(this.derection === 1){
+        circle(this.x+30,this.y+10,20);
+      }
+    }
   }
   move(){
   //updates x pos. baces on xspeed. 
   //If vehicle goes off screen its moved to the other side
+    if(this.derection === 0){
+      this.x-= this.s;
+      if(this.x<0){
+        this.x += width;
+      }
+    }
+    if(this.derection === 1){
+      this.x+= this.s;
+      if(this.x>width){
+        this.x -= width;
+      }
+    }
   }
   speedUp(){
   //speeding up vehicle
+    
+      
   }
   speedDown(){
   //slowing down vehicle
@@ -54,6 +106,9 @@ class Vehicle{
   //changing color 
   }
   action(){
+    this.display();
+    this.move();
+    this.speedUp();
   // calls all functions when needed
   }
 }
