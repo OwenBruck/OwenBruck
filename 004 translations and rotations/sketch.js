de3acdd0c47ae8284538695b9509c4f57a239d9a
OwenBruck <145162288+OwenBruck@users.noreply.github.com>
2023-10-23 15:18:05 -0600

    1

---
+++ 004 translations and rotations/sketch.js
@@ -0,0 +1,46 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+frameCount(10);
+function draw() {
+  background(220);
+  circle(width/2,height/2,500);
+  push();
+  translate(width/2,height/2);
+  
+  for(let i = 0; i < 12; i++){
+    strokeWeight(3);
+    point(0,0);
+    rotate(radians(30));
+    line(250, 0, 200, 0);
+
+  }
+  for(let i = 0; i < 60; i++){
+    strokeWeight(2);
+    rotate(radians(6));
+    line(250, 0, 230, 0);
+
+  }
+  
+  let rotationS=6;
+  rotate(radians(rotationS));
+  line(0,0,100,0);
+  rotationS += 1;
+  
+
+  
+
+
+
+  pop();
+}
+
