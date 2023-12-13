10f966664031dbec4921a911109540946d14a4e5
OwenBruck <145162288+OwenBruck@users.noreply.github.com>
2023-12-12 22:03:16 -0600

    replaced the place holder sprites with actual sprites

--- Final Capstone/sketch.js
+++ Final Capstone/sketch.js
@@ -8,11 +8,26 @@ let shipSpeed = 10;
 let aliens = [];
 let bullets = [];
 let gameOver = false;
-let backgroundColor = 150;
+let backgroundColor = 20;
 let score = 0;
-let alienGap = 40;
+let alienGap = 100;
+let alien00;
+let alien10;
+let alien20;
+let alien01;
+let alien11;
+let alien21;
+let ship0;
 
-
+function preload(){
+  alien00 = loadImage("assets/alien00.png");
+  alien10 = loadImage("assets/alien10.png");
+  alien20 = loadImage("assets/alien20.png");
+  alien01 = loadImage("assets/alien01.png");
+  alien11 = loadImage("assets/alien11.png");
+  alien21 = loadImage("assets/alien21.png");
+  ship0 = loadImage("assets/ship.png");
+}
 
 function setup() {
   createCanvas(800, 800);
@@ -50,22 +65,22 @@ function endGame(){
 
 function drawShip(x){
   fill("white");
-  rect(x, 750, 40, 25); 
+  image(ship0,x,650,ship0.width+15, ship0.height+10)
 }
 
 function generateAliens(){
   let tempArray =[];
-  for(let y = alienGap; y <= alienGap * 5; y += 40){
+  for(let y = 40; y <= 200; y += 40){
     tempArray = [];
-    for(let x = 40; x <= 440 ; x +=40){
+    for(let x = 50; x <= 550 ; x +=50){
       if(y===40){
-        tempArray.push(new Alien(x,y,2));
+        tempArray.push(new Alien(x,y+alienGap,2));
       }
       else if (y <= 120){
-        tempArray.push(new Alien(x,y,1));
+        tempArray.push(new Alien(x,y+alienGap,1));
       }
       else{
-        tempArray.push(new Alien(x,y,0));
+        tempArray.push(new Alien(x,y+alienGap,0));
       }
     }  
     aliens.push(tempArray); 
@@ -92,27 +107,27 @@ class Alien{
     this.type = type;
     this.x = x;
     this.y = y;
-    this.speed = 20;
+    this.speed = 10;
     this.needToDrop = 0;
   }
   display(){
     if (this.type === 0){
       fill("red");
-      square(this.x,this.y,35);
+      image(alien00,this.x,this.y,alien00.width)
     }
 
     else if (this.type === 1){
       fill("blue");
-      square(this.x,this.y,35);
+      image(alien10,this.x,this.y,alien10.width+5)
     }
 
     else {
       fill("green");
-      square(this.x,this.y,35);
+      image(alien20,this.x + 6,this.y,alien20.width+3)
     }
   }
   move(){
-    if(frameCount % 25 === 0){
+    if(frameCount % 30 === 0){
       
       if(this.needToDrop === 1){
         this.y+=40;
@@ -126,8 +141,6 @@ class Alien{
   changeDirection(){
     this.speed *= -1;
     this.needToDrop = 1;
-    // this.y += 40; 
-    // this.x -= 20;
    
   }
 
